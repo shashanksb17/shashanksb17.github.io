d8a8e0096c512444fbdbef004b23097ee30ce7ad
shashanksb17 <sbasutkar999@gmail.com>
2023-03-15 21:04:56 +0530

    changes

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-// ------------back-to-top------------
-
-
-
+window.onscroll = function() {myFunction()};
+    
+    var navbar = document.getElementById("nav-menu");
+    var sticky = navbar.offsetTop;
+    
+    function myFunction() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
 
 
