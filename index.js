fd5a1bfb1fc46b0fd36cdc9b5b6983db4e4d7ac4
shashanksb17 <sbasutkar999@gmail.com>
2023-05-09 17:17:33 +0530

    changes

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 window.onscroll = function() {myFunction()};
     
-    var navbar = document.getElementById("nav-menu");
-    var sticky = navbar.offsetTop;
-    
-    function myFunction() {
-      if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-      } else {
-        navbar.classList.remove("sticky");
-      }
-    }
+var navbar = document.getElementById("nav-menu");
+var sticky = navbar.offsetTop;
 
-    function resume(){
-      window.open("https://drive.google.com/file/d/11_-r_h6_JgUhM2Dp9SmyMQJFasghLrMJ/view?usp=share_link","_blank")
-    }
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+function resume(){
+  window.open("https://drive.google.com/file/d/11_-r_h6_JgUhM2Dp9SmyMQJFasghLrMJ/view?usp=share_link","_blank")
+}
 
